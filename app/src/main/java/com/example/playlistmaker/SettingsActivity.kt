b71de9c3d2539385a26d2f1afad725fbeeb6e2f0
Hesoyam <ashot.m1985@gmail.com>
2025-05-18 22:44:11 +0300

    init commit

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -7,14 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backspaceLibraryButton = findViewById<ImageView>(R.id.settings_backspace_button)
-        backspaceLibraryButton.setOnClickListener {
+        val settingsToolbar = findViewById<MaterialToolbar>(R.id.settings_toolbar)
+        settingsToolbar.setNavigationOnClickListener {
             finish()
         }
 
