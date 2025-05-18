69edd29ba9854275850e78a052362e326594cd3e
Hesoyam <ashot.m1985@gmail.com>
2025-05-18 04:30:02 +0300

    init commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -23,5 +24,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         val libraryButton = findViewById<Button>(R.id.library_button)
+        val libraryButtonClickListener : View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "Медиатека временно не работает.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        libraryButton.setOnClickListener(libraryButtonClickListener)
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Настройки временно не работают.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 }
