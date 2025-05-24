e772932af21434b419198faa6fb90faf2acf5036
Hesoyam-01 <ashot.m1985@gmail.com>
2025-05-24 05:01:35 +0300

    init commit

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -5,11 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
+
+        val searchToolbar = findViewById<MaterialToolbar>(R.id.search_toolbar)
+
+        searchToolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
