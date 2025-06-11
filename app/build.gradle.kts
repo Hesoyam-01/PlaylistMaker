311c84f2d15ef6ed8930b070ab3152d2e0ddadc5
Hesoyam-01 <ashot.m1985@gmail.com>
2025-06-11 22:47:02 +0300

    init commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.material.v190)
+    implementation(libs.glide)
 }
