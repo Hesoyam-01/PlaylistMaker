06d5bc9f73b857814a70bed9d2ec5ff304e1251d
Hesoyam-01 <ashot.m1985@gmail.com>
2025-09-19 01:43:07 +0300

    init commit

--- app/src/main/java/com/example/playlistmaker/domain/api/SearchHistory.kt
+++ app/src/main/java/com/example/playlistmaker/domain/api/SearchHistory.kt
@@ -1,9 +1,11 @@
 package com.example.playlistmaker.domain.api
 
 import com.example.playlistmaker.domain.models.Track
+import com.example.playlistmaker.ui.search.TrackAdapter
 
 interface SearchHistory {
     val lastTracksList: MutableList<Track>
+    val lastTrackAdapter: TrackAdapter
     fun saveLastTrackList()
     fun addToLastTrackList(track: Track)
     fun loadLastTrackList()
