1ae3a892e80d8791b78c97d77262d4df02af26a0
Hesoyam-01 <ashot.m1985@gmail.com>
2025-09-29 08:45:36 +0300

    init commit

--- app/src/main/java/com/example/playlistmaker/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/player/PlayerActivity.kt
@@ -11,7 +11,6 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityPlayerBinding
-import com.example.playlistmaker.presentation.player.MediaState
 import com.example.playlistmaker.presentation.player.PlayerViewModel
 
 class PlayerActivity : AppCompatActivity() {
