d621840782046abb1f59557c266ad38e11abddc6
Hesoyam-01 <ashot.m1985@gmail.com>
2025-10-08 13:33:08 +0300

    init commit

--- app/src/main/java/com/example/playlistmaker/data/impl/player/MediaRepositoryImpl.kt
+++ app/src/main/java/com/example/playlistmaker/data/impl/player/MediaRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.example.playlistmaker.data.impl.player
 
 import android.media.MediaPlayer
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.playlistmaker.domain.api.player.MediaRepository
@@ -15,10 +14,8 @@ class MediaRepositoryImpl(private val mediaPlayer: MediaPlayer) : MediaRepositor
     override fun prepare(previewUrl: String) {
         mediaPlayer.setDataSource(previewUrl)
         mediaPlayer.setOnPreparedListener {
-            Log.d("end", "end")
             mediaStateLiveData.postValue(MediaState.PREPARED)
         }
-        Log.d("start", "start")
         mediaPlayer.prepareAsync()
         mediaPlayer.setOnCompletionListener {
             mediaStateLiveData.postValue(MediaState.PREPARED)
